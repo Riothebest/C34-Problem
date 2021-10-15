d157ef97b9708cc9fa8e9dcd2afa7561cc5651f3
Riothebest <86832131+Riothebest@users.noreply.github.com>
2021-10-15 17:05:34 +0530

    Add files via upload

---
+++ js/bullet.js
@@ -0,0 +1,81 @@
+class Bullet
+{
+    constructor(x,y,w,h)
+    {
+        var options ={
+            isStatic: true,
+            friction: 1,
+            density: 1,
+        }
+        this.image = loadImage('assets/laser.png');
+        this.body = Bodies.rectangle(x,y,w,h,options)
+        this.w = w;
+        this.h = h
+        
+
+        //flag value
+       
+        this.speed = 0.05;
+        this.animation = [this.image];
+
+        World.add(myWorld, this.body);
+    }
+
+    animate()
+    {
+        this.speed +=0.05;
+    }
+
+    remove(index)
+    {
+         this.isSink = true;
+         Matter.Body.setVelocity(this.body, {x:0,y:0});
+
+        
+         this.speed = 0.05;
+        // this.r =   150;
+         
+         setTimeout(()=>
+         {
+             Matter.World.remove(myWorld,this.body);
+            // boats.splice(index,1);
+            delete bullets[index];
+         },1000)
+
+
+
+    }
+
+    shoot()
+    {
+       
+       var loc = p5.Vector.fromAngle(sentry.angle);
+
+       loc.mult(18);
+
+       Body.setStatic(this.body, false);
+       Body.setVelocity(this.body,{x:loc.x, y:loc.y});
+
+    }
+
+    display()
+    {
+        var pos= this.body.position;
+        var angle = this.body.angle;
+
+        var index = floor(this.speed % this.animation.length)
+        //1.4 = 1, 1.9 = 1
+        //ceil : 1.4 = 2 1.9 =2
+        push();
+        
+        translate(pos.x, pos.y)
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.animation[index],0,0,this.w, this.h);
+        pop();
+
+       
+
+      
+    }
+}
